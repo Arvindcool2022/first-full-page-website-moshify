54167a24507e49166482c9276974a96122c9a881
Arvind A <arvindcool2022@gmail.com>
2022-07-28 23:37:33 +0530

    added Collapsibles and js folder.
    1. added Collapsibles with animation
    2. added svg icon instead of sprite
    3. class = collapse (not Collapsibles)
    4. edited js file to change Collapsibles into collapse

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const collapse = document.querySelectorAll(".collapse");
+collapse.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapse--expanded");
+  })
+);
